e7a7bbbd5899b17a7ee3e98976f465b9954ab0a2
Loue143 <louegidalag12@gmail.com>
2025-07-31 22:38:10 +0800

    Kwaa

--- src/Java/Activity2.java
+++ src/Java/Activity2.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package lowi;
+package Java;
 
-/**
- *
- * @author PC3
- */
 public class Activity2 {
     
 }
