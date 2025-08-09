435bf9c28d405d38c7149b23bf25adfa507b8308
Loue143 <louegidalag12@gmail.com>
2025-08-09 16:29:02 +0800

    Assignment na sir

--- src/Banking/BankingClass.java
+++ src/Banking/BankingClass.java
@@ -1,32 +1,61 @@
 package Banking;
 
-import java.util.Scanner;
-
 public class BankingClass {
-    
-    int accountNo = 10001;
-    String name;
-    float initialBalance = 0;
-    int pin = 1234;
-    
-    public boolean verifyAccount(int acc, int pn){        
-       
-            if(acc == accountNo && pn == pin){
-                return true;
-            }else{
-                return false;
-            }
-       
-    }
-    public int setAccount(){
-        return accountNo;
+
+    private int accNo;
+    private float balance;
+    private int pin;
+
+    // Getters
+    public float getBalance() {
+        return balance;
     }
 
-    public void viewBalance(){
-    
-    
+    public int getPin() {
+        return pin;
     }
-    
-    
-    
+
+    public int getAccNo() {
+        return accNo;
+    }
+
+    // Setters
+    public void setBalance(float balance) {
+        this.balance = balance;
+    }
+
+    public void setPin(int pin) {
+        this.pin = pin;
+    }
+
+    public void setAccNo(int accNo) {
+        this.accNo = accNo;
+    }
+
+    // Verify account details
+    public boolean verifyAccount(int accn, int pin) {
+        return (this.accNo == accn && this.pin == pin);
+    }
+
+    // Deposit money
+    public void deposit(float amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposit successful. New balance: " + balance);
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+    // Withdraw money
+    public void withdraw(float amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawal successful. New balance: " + balance);
+        } else if (amount > balance) {
+            System.out.println("Insufficient funds.");
+        } else {
+            System.out.println("Invalid withdrawal amount.");
+        }
+    }
 }
